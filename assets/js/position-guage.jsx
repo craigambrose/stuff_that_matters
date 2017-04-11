5b117a8b9d2c66cff669c4e3735446be9b42b148
craigambrose <craig@craigambrose.com>
2017-04-11 19:34:12 +0800

    added chart.js

---
+++ assets/js/position-guage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Doughnut} from 'react-chartjs-2'
+
+function PosisionGuage(props) {
+  var data = {
+      labels: [
+          "Blue",
+          "Yellow"
+      ],
+      datasets: [
+          {
+              data: [50, 100],
+              backgroundColor: [
+                  "#36A2EB",
+                  "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                  "#36A2EB",
+                  "#FFCE56"
+              ]
+          }]
+  }
+
+  const options = {
+    rotation: -Math.PI,
+    circumference: Math.PI,
+    legend: {
+      display: false
+    }
+  }
+
+  return (
+    <div>
+      <Doughnut data={data} options={options} />
+    </div>
+  )
+}
+
+export default PosisionGuage
--- assets/js/widget-loaders.jsx
+++ assets/js/widget-loaders.jsx
@@ -1,11 +1,18 @@
 import ArgumentCards from './argument-cards'
+import PositionGuage from './position-guage'
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-document.renderWidget = (nodeID) => {
+const widgets = {
+  argumentCards: ArgumentCards,
+  positionGuage: PositionGuage
+}
+
+document.renderWidget = (widgetName, nodeID) => {
+  const widget = widgets[widgetName]
   ReactDOM.render(
-    React.createElement(ArgumentCards),
+    React.createElement(widget),
     document.getElementById(nodeID)
   );
 }
